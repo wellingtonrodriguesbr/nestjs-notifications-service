30cb3e3015134595b006183aebb9d260a4ff341d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-05 07:32:31 -0300

    feat: project structure

---
+++ prisma/migrations/20240704215535_create_notifications/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "recipient_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "read_at" DATETIME,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE INDEX "notifications_recipient_id_idx" ON "notifications"("recipient_id");
