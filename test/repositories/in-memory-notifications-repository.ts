4d696b9cd99e9eb103955b87eb4edd4dc61b8cee
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-07 11:25:47 -0300

    feat: create save, findById and countManyByRecipientId methods in Notifications repository

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,30 @@ export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
   public items: Notification[] = [];
+
   async create(notification: Notification) {
     this.items.push(notification);
+  }
+
+  async save(notification: Notification) {
+    const notificationIndex = this.items.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    this.items[notificationIndex] = notification;
+  }
+
+  async findById(notificationId: string) {
+    const notification = this.items.find((item) => item.id === notificationId);
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+
+  async countManyByRecipientId(recipientId: string) {
+    return this.items.filter((item) => item.recipientId === recipientId).length;
   }
 }
